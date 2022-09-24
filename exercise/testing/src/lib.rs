3c9fb86c2fd90e90963c5884aeb40d66c5480869
Samuel Porras <Arkiant@users.noreply.github.com>
2022-09-24 15:03:11 +0200

    feat: add unit test and doc test

--- exercise/testing/src/lib.rs
+++ exercise/testing/src/lib.rs
@@ -1,3 +1,12 @@
+
+/// Returns the sum of x and y and the subtraction of z
+/// # Example
+/// 
+/// ```
+/// # use testing::sploosh;
+/// let result = sploosh(1,2,3);
+/// assert_eq!(result, 4);
+/// ```
 pub fn sploosh(x: i32, y: i32, z: i32) -> i32 {
     match (x, y, z) {
         (x, _, _) if x < 0 => 99,
@@ -7,15 +16,26 @@ pub fn sploosh(x: i32, y: i32, z: i32) -> i32 {
     }
 }
 
+
+/// Returns a in negative plus 3 for b
+/// # Example
+/// 
+/// ```
+/// # use testing::splish;
+/// let result = splish(9,3);
+/// assert_eq!(result, 0);
+/// ```
 pub fn splish(a: i32, b: i32) -> i32 {
     -a + 3 * b
 }
 
 // 1. Use the `cfg` attribute to mark the `test` module below as a test module
-
+#[cfg(test)]
 mod test {
     // 2. Bring all the library items into scope with a `use` statement
     // Hint: It's okay to use `*` here.
+
+    use super::*;
 
     // 3. Write a test function that verifies the following condition using the `assert_eq!` or
     // `assert_ne!` macros
@@ -25,11 +45,22 @@ mod test {
     //
     // `cargo test` should run your tests and pass
     // Hint: Don't forget the `#[test]` attribute for your test function!
+    #[test]
+    fn sploosh_result_validation() {
+        assert_eq!(sploosh(1, 2, 3), 4);
+        assert_ne!(sploosh(5, 6, 7), 4);
+    }
 
     // 4. Write a test function that verifies the following conditions using the `assert!` macro
     // - splish(100, 10) is negative
     // - splish(40, 20) is positive
     // - splish(9, 3) is 0
+    #[test]
+    fn splish_result_validation() {
+        assert!(splish(100, 10) < 0);
+        assert!(splish(40, 20) > 0);
+        assert!(splish(9, 3) == 0);
+    }
 }
 
 // 5. Create a `tests/` directory and an integration test file `tests/more_tests.rs`
